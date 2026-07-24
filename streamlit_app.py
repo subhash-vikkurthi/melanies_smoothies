5cf2eaa7f2212f959f7452765ad35076970733d5
subhash-vikkurthi <subhashv9966@gmail.com>
2026-07-24 14:05:06 +0530

    Comment out dataframe display and stop
    Comment out dataframe display and stop functions.

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingrediants_list = st.multiselect(
     'choose_upto 5 fruits:',
@@ -37,6 +37,9 @@ if ingrediants_list:
     ingredients_string = ""
     for fruits_choosen in ingrediants_list:
         ingredients_string += fruits_choosen +' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+      
         st.subheader(fruits_choosen + " Nutrition Information")
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruits_choosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
