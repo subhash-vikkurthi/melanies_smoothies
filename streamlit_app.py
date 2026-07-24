c0fe20cc2d6e926207a06b3b1c4ddd1449e881f3
subhash-vikkurthi <subhashv9966@gmail.com>
2026-07-24 14:09:54 +0530

    Fix variable name in ingredient loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,8 +37,8 @@ if ingrediants_list:
     ingredients_string = ""
     for fruits_choosen in ingrediants_list:
         ingredients_string += fruits_choosen +' '
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_choosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_choosen,' is ', search_on, '.')
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruits_choosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruits_choosen,' is ', search_on, '.')
       
         st.subheader(fruits_choosen + " Nutrition Information")
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruits_choosen)
